8ffd0ce6a2d346fa02d39d23bcf3fba6e3a87e28
LEBRONGOAT2323 <hrsipes22@gmail.com>
2026-03-04 16:33:13 -0500

    Create renderer.js

---
+++ renderer.js
@@ -0,0 +1,9 @@
+function go() {
+  let url = document.getElementById("url").value;
+
+  if (!url.startsWith("http")) {
+    url = "https://" + url;
+  }
+
+  document.getElementById("browser").src = url;
+}
